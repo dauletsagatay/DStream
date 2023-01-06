d84f013018935a151ba839691df64019be3ab7fc
Daulet Sagatay <d.sagatay@imbc.kz>
2023-01-06 12:11:33 +0600

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,32 @@
+import { Stack, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+// import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
+import logo from "../utils/logo.png";
+
+const Navbar = () => {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#19181A",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={40} />
+        {/* <Typography variant="h4" fontWeight="bold" color="#FC1503">
+          DStream
+        </Typography> */}
+      </Link>
+      <SearchBar />
+    </Stack>
+  );
+};
+
+export default Navbar;
